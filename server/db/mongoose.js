76c4f1061e91c25005eb9323692c1b34620884ae
Carlos Pinho <carlos@Carlos-Pinhos-MacBook-Air.local>
2017-04-13 11:32:07 +0300

    Got mlab running

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
-var uri = 'mongodb://carlos:nikita@ds159050.mlab.com:59050/todoapi'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(uri || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
